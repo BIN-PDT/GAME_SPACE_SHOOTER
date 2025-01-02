7c881837b95c98cdfb9b8b48e8cd9bf3815e8af3
BIN BIN <thangpd300502@gmail.com>
2025-01-02 16:13:11 +0700

    BASIC LASER

--- source/2D/main.py
+++ source/2D/main.py
@@ -1,6 +1,6 @@
 from settings import *
 from timers import Timer
-from sprites import Player
+from sprites import Player, Laser
 
 
 class Game:
@@ -8,14 +8,18 @@ class Game:
         init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Space Shooter")
         self.load_assets()
 
+        self.group_laser = []
         self.player = Player(
-            self.assets["player"], Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+            self.assets["player"],
+            Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2),
+            self.shoot_laser,
         )
 
     def load_assets(self):
         self.assets = {
             "player": load_texture(join("images", "spaceship.png")),
             "star": load_texture(join("images", "star.png")),
+            "laser": load_texture(join("images", "laser.png")),
         }
 
         self.star_data = [
@@ -32,15 +36,26 @@ class Game:
         for star in self.star_data:
             draw_texture_ex(self.assets["star"], star[0], 0, star[1], WHITE)
 
+    def shoot_laser(self, pos):
+        self.group_laser.append(Laser(self.assets["laser"], pos))
+
+    def discard_sprites(self):
+        self.group_laser = [laser for laser in self.group_laser if not laser.discard]
+
     def update(self):
         dt = get_frame_time()
         self.player.update(dt)
+        self.discard_sprites()
+        for laser in self.group_laser:
+            laser.update(dt)
 
     def draw(self):
         begin_drawing()
         clear_background(BG_COLOR)
         self.draw_stars()
         self.player.draw()
+        for laser in self.group_laser:
+            laser.draw()
         end_drawing()
 
     def run(self):
--- source/2D/sprites.py
+++ source/2D/sprites.py
@@ -8,10 +8,14 @@ class Sprite:
         self.pos = pos
         self.direction = direction
         self.SPEED = speed
+        self.discard = False
 
     def move(self, dt):
         self.pos.x += self.direction.x * self.SPEED * dt
         self.pos.y += self.direction.y * self.SPEED * dt
+
+    def check_discard(self):
+        self.discard = not -300 < self.pos.y < WINDOW_HEIGHT + 300
 
     def update(self, dt):
         pass
@@ -21,8 +25,9 @@ class Sprite:
 
 
 class Player(Sprite):
-    def __init__(self, texture, pos):
+    def __init__(self, texture, pos, shoot_laser):
         super().__init__(texture, pos, Vector2(), PLAYER_SPEED)
+        self.shoot_laser = shoot_laser
 
     def input(self):
         self.direction.x = int(is_key_down(KEY_RIGHT)) - int(is_key_down(KEY_LEFT))
@@ -30,7 +35,7 @@ class Player(Sprite):
         self.direction = Vector2Normalize(self.direction)
 
         if is_key_pressed(KEY_SPACE):
-            print("Laser")
+            self.shoot_laser(Vector2(self.pos.x + self.size.x / 2, self.pos.y - 60))
 
     def constraint(self):
         self.pos.x = max(0, min(self.pos.x, WINDOW_WIDTH - self.size.x))
@@ -40,3 +45,12 @@ class Player(Sprite):
         self.input()
         self.move(dt)
         self.constraint()
+
+
+class Laser(Sprite):
+    def __init__(self, texture, pos):
+        super().__init__(texture, pos, Vector2(0, -1), LASER_SPEED)
+
+    def update(self, dt):
+        self.move(dt)
+        self.check_discard()
